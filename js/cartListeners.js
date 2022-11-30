f96622ae11a8b45accf61f4067c2d217f00c65ab
KiditsGood <71182254+KiditsGood@users.noreply.github.com>
2022-11-30 11:18:20 +0300

    order_check_auth ready

--- js/cartListeners.js
+++ js/cartListeners.js
@@ -1,15 +1,54 @@
+const orderDelivery = $('.order__delivery')
+
 $('.order__buttons-item').click(function () {
     $(this).addClass('active').siblings().removeClass('active')
 })
 
-$('.self').click(() => {
-    $('.order__delivery').css('display', 'none').prev().css('display', 'flex')
+$('.company__field-item').click(function () {
+    $(this).addClass('active').siblings().removeClass('active')
+})
 
-    $('.order__delivery').find('.order__input').attr('disabled', '')
+$('.send__buttons-item').click(function () {
+    $(this).addClass('active').siblings().removeClass('active')
+})
+
+$('.self').click(() => {
+    orderDelivery.css('display', 'none').prev().css('display', 'flex')
+    orderDelivery.next().css('display', 'none')
 })
 
 $('.delivery').click(() => {
-    $('.order__delivery').css('display', 'flex').prev().css('display', 'none')
+    orderDelivery.css('display', 'flex').prev().css('display', 'none')
+    orderDelivery.next().css('display', 'none')
+})
 
-    $('.order__delivery').find('.order__input').removeAttr('disabled')
+$('.courier').click(() => {
+    orderDelivery.css('display', 'none').prev().css('display', 'none')
+    orderDelivery.next().css('display', 'flex')
+})
+
+$(document).ready(function(){
+    if($(window).width() < 1000) {
+        $('.cart__order-check').html('Подтвердить')
+    }
+
+    $('.pointer__left-input').each(function () {
+        $(this).on('keyup', function () {
+            let value = $(this).val().toLowerCase()
+
+            $(this).next().find($('.variable__lable')).filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        })
+    })
+});
+
+$('.to_take').click(function () {
+    $(this).parent().parent().parent().find('.del__address').css('display', 'none')
+    $(this).parent().parent().next().css('display', 'flex')
+})
+
+$('.to_self').click(function () {
+    $(this).parent().parent().parent().find('.del__address').css('display', 'flex')
+    $(this).parent().parent().next().css('display', 'none')
 })
